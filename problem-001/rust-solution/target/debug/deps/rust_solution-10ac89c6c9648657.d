31c905a154a48f8b1f1b6786f90e551a9ea8a8d3
Nathan <nathanroark@protonmail.com>
2022-08-27 20:40:44 -0500

    updated gitignore

--- problem-001/rust-solution/target/debug/deps/rust_solution-10ac89c6c9648657.d
+++
@@ -1,5 +0,0 @@
-/home/nathan/Code/project-euler/problem-001/rust-solution/target/debug/deps/rust_solution-10ac89c6c9648657: src/main.rs
-
-/home/nathan/Code/project-euler/problem-001/rust-solution/target/debug/deps/rust_solution-10ac89c6c9648657.d: src/main.rs
-
-src/main.rs:
--- problem-001/rust-solution/target/debug/rust-solution.d
+++
@@ -1,1 +0,0 @@
-/home/nathan/Code/project-euler/problem-001/rust-solution/target/debug/rust-solution: /home/nathan/Code/project-euler/problem-001/rust-solution/src/main.rs
